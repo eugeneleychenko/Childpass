aafbc34cbc8e86bcd850b3981d191779dda1e12e
AndreyV <andrei-vinogradov@yandex.ru>
2014-04-23 13:04:50 +0400

    Refs #3369: Edit children action optimization
    --HG--
    branch : features

--- protected/controllers/ChildController.php
+++ protected/controllers/ChildController.php
@@ -30,11 +30,19 @@ class ChildController extends Controller
 
     public function actionAdd($step = 'step1')
     {
+        $form = new CForm('application.views.child.add' . ucfirst($step) . 'Form');
+
+        $childId = isset($_GET['child_id']) ? $_GET['child_id'] : false;
+
+        $data = array();
+
         switch ($step) {
             case 'step1':
-                $form = new CForm('application.views.child.add' . ucfirst($step) . 'Form');
-
-                $form['child']->model = new Child();
+                if ($childId) {
+                    $form['child']->model = Child::model()->findByPk($childId);
+                } else {
+                    $form['child']->model = new Child();
+                }
 
                 if ($form->submitted('addStep1')) {
                     $form['child']->model->user_id = Yii::app()->user->getId();
@@ -45,23 +53,11 @@ class ChildController extends Controller
                     }
                 }
 
-                $this->render(
-                    'add' . ucfirst($step), array(
-                        'form' => $form,
-                    )
-                );
                 break;
             case 'step2':
-                $form = new CForm('application.views.child.add' . ucfirst($step) . 'Form');
-
                 $form['child']->model = new ChildPhoto();
-                $childId = $_GET['child_id'];
 
-                $this->render(
-                    'add' . ucfirst($step), array(
-                        'form' => $form,
-                    )
-                );
+                $childId = $_GET['child_id'];
 
                 if ($form->submitted('addStep2') && $childId) {
                     $images = CUploadedFile::getInstances($form['child']->model, 'image');
@@ -85,23 +81,29 @@ class ChildController extends Controller
                         }
                     }
                     $this->redirect(array('child/add/step3'));
+                } else {
+                    $childPhotos = ChildPhoto::model()->findAll('child_id = :child_id', array(':child_id' => $childId));
+                    foreach ($childPhotos as $photo) {
+                        $photo->filename = Yii::app()->request->getBaseUrl(true).'/children/'.$childId.'/photos/'.$photo->filename;
+                    }
+                    $data['childPhotos'] = $childPhotos;
                 }
 
                 break;
             case 'step3':
-                $form = new CForm('application.views.child.add' . ucfirst($step) . 'Form');
-
                 $form['child']->model = new Child();
 
-                $this->render(
-                    'add' . ucfirst($step), array(
-                        'form' => $form,
-                    )
-                );
                 break;
             case 'step4':
                 break;
         }
+
+        $this->render(
+            'add' . ucfirst($step), array(
+                'form' => $form,
+                'data' => $data
+            )
+        );
     }
 
     public function actionList()
@@ -114,7 +116,7 @@ class ChildController extends Controller
                 'joinType' => 'LEFT JOIN',
                 'order' => 'is_main DESC'
             ),
-        ))->findAll('user_id = :user_id', array(':user_id' => $userId));
+            ))->findAll('user_id = :user_id', array(':user_id' => $userId));
 
         foreach ($childList as &$child) {
             if (isset($child->childPhotos[0])) {
@@ -134,80 +136,6 @@ class ChildController extends Controller
                 'childList' => $childList,
             )
         );
-    }
-
-    public function actionEdit($step = 'step1')
-    {
-        if($childId = $_GET['child_id']) {
-            switch ($step) {
-                case 'step1':
-                    $form = new CForm('application.views.child.addStep1Form');
-
-                    $form['child']->model = Child::model()->findByPk($childId);
-
-                    if ($form->submitted('addStep1')) {
-                        $form['child']->model->user_id = Yii::app()->user->getId();
-                        if ($form->validate()) {
-                            if ($form['child']->model->save(false)) {
-                                $this->redirect(array('/child/edit/step2', 'child_id' => $form['child']->model->id));
-                            }
-                        }
-                    }
-
-                    $this->render(
-                        'addStep1', array(
-                            'form' => $form,
-                            'mode' => 'edit'
-                        )
-                    );
-                    break;
-                case 'step2':
-                    $form = new CForm('application.views.child.add' . ucfirst($step) . 'Form');
-
-                    $form['child']->model = new ChildPhoto();
-
-                    $childPhotos = ChildPhoto::model()->findAll('child_id = :child_id', array(':child_id' => $childId));
-                    foreach ($childPhotos as $photo) {
-                        $photo->filename = Yii::app()->request->getBaseUrl(true).'/children/'.$childId.'/photos/'.$photo->filename;
-                    }
-
-                    $this->render(
-                        'editStep2', array(
-                            'form' => $form,
-                            'childPhotos' => $childPhotos
-                        )
-                    );
-
-                    if ($form->submitted('addStep2')) {
-                        $images = CUploadedFile::getInstances($form['child']->model, 'image');
-
-                        if (!empty($images)) {
-                            $savePath = Yii::getPathOfAlias('webroot') . "/children/{$childId}/photos/";
-                            if (!is_dir($savePath)) mkdir($savePath, 0777, true);
-                            foreach ($images as $image => $pic) {
-
-                                if ($pic->saveAs($savePath . $pic->name)) {
-                                    $photo           = $form['child']->model;
-                                    $photo->image    = $pic;
-                                    $photo->filename = $pic->name;
-                                    $photo->child_id = $childId;
-                                    $photo->save();
-                                } else {
-                                    $form['child']->model->addError;
-
-                                    echo 'Cannot upload!';
-                                }
-                            }
-                        }
-                        $this->redirect(array('child/edit/step3'));
-                    }
-                    break;
-                case 'step3':
-                    break;
-                case 'step4':
-                    break;
-            }
-        }
     }
 
 }
