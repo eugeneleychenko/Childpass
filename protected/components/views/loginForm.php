85f2cd79cdd18ad1d8c91181dfc1a7c794ca7e31
AndreyV <andrei-vinogradov@yandex.ru>
2014-04-25 17:48:24 +0400

    Refs #3362: Change login form, main menu fix

--- protected/components/views/loginForm.php
+++ protected/components/views/loginForm.php
@@ -15,23 +15,24 @@
             )
         )); ?>
         <?php echo $form->errorSummary($model, ''); ?>
-        <div style="float: left; width: 50px;"><h6 style="color: #002f65; font-weight: 500;">Log In</h6></div>
-        <input type="hidden" name="csrf_test_name" value="cee0db86402bed7618a8f9daa3a194cc">
-        <div style="float: left; width: 136px;">
-            <?php echo $form->textField($model,'username', array('placeholder' => 'Email', 'style' => 'border: 1px solid grey; font-size: 15px; width: 111px; height: 16px; padding-left: 8px; margin-bottom: 5px;')) ?>
-            <?php echo $form->passwordField($model,'password', array('placeholder' => 'Password', 'style' => 'border: 1px solid grey; width: 111px; height: 16px; font-size: 15px; padding-left: 8px;')) ?>
+        <div class="login-label">Log In</div>
+        <div class="login-fields">
+            <?php echo $form->textField($model,'username', array('placeholder' => 'Email')) ?>
+            <?php echo $form->passwordField($model,'password', array('placeholder' => 'Password')) ?>
             <?php echo $form->hiddenField($model,'remember', array('value' => true)) ?>
         </div>
-        <div style="float: left; width: 103px;">
+        <div class="login-buttons">
             <?php echo CHtml::submitButton('login', array('class' => 'btn', 'name' => 'submit', 'value' => 'go»')); ?>
             <a class="forgot-password" href="<?php echo $this->createUrl('user/forgotPassword') ?>">Forgot password?</a>
         </div>
         <?php $this->endWidget();
     } else {
         ?>
-        <p>Welcome, <span><?php echo Yii::app()->user->getName() ?></span></p>
+        <div class="logout-buttons">
+            <p class="welcome-label">Welcome, <span><?php echo Yii::app()->user->getName() ?></span></p>
+            <?php echo CHtml::link('log out»', array('user/logout'), array('class' => 'btn logout')); ?>
+        </div>
         <?php
-        echo CHtml::link('log out»', array('user/logout'));
     }
     ?>
 </div><!-- form -->
