223dc081cd0f8f3d11b82cce0a0f02cf2a0e6fc9
Oleg Proshin <opp20071980@gmail.com>
2014-05-29 18:05:47 +0400

    Refs #3602: Add child profile Step1 - add data for storing relations
    --HG--
    branch : features

---
+++ protected/migrations/m140529_132503_relative.php
@@ -0,0 +1,83 @@
+<?php
+
+class m140529_132503_relative extends CDbMigration
+{
+
+
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+        $this->createTable('{{relation}}', array(
+            'id'                => 'pk',
+            'name'              => 'nvarchar(50) NOT NULL',
+        ));
+
+        $this->insert('{{relation}}', array(
+                'id' => 1,
+                'name' => 'Father')
+        );
+        $this->insert('{{relation}}', array(
+                'id' => 2,
+                'name' => 'Mother')
+        );
+        $this->insert('{{relation}}', array(
+                'id' => 3,
+                'name' => 'Grandfather')
+        );
+        $this->insert('{{relation}}', array(
+                'id' => 4,
+                'name' => 'GrandMother')
+        );
+        $this->insert('{{relation}}', array(
+                'id' => 5,
+                'name' => 'Uncle')
+        );
+        $this->insert('{{relation}}', array(
+                'id' => 6,
+                'name' => 'Aunt')
+        );
+
+        $this->createTable('{{relative}}', array(
+            'id'                => 'pk',
+            'first_name'              => 'nvarchar(100) NOT NULL',
+            'last_name'              => 'nvarchar(100) NOT NULL',
+        ));
+
+
+        $this->createTable('{{child_relative}}', array(
+            'id'                => 'pk',
+            'child_id'          => 'integer NOT NULL',
+            'relative_id'       => 'integer NOT NULL',
+            'relation_id'       => 'integer NOT NULL'
+        ));
+
+        $this->addForeignKey('{{child_relative}}_{{relative}}_FK',
+            '{{child_relative}}',
+            'relative_id',
+            '{{relative}}',
+            'id'
+        );
+
+        $this->addForeignKey('{{child_relative}}_{{child}}_FK',
+            '{{child_relative}}',
+            'child_id',
+            '{{child}}',
+            'id'
+        );
+
+        $this->addForeignKey('{{child_relative}}_{{relation}}_FK',
+            '{{child_relative}}',
+            'relation_id',
+            '{{relation}}',
+            'id'
+        );
+    }
+
+	public function safeDown()
+	{
+        $this->dropTable('{{child_relative}}');
+        $this->dropTable('{{relation}}');
+        $this->dropTable('{{relative}}');
+	}
+
+}
