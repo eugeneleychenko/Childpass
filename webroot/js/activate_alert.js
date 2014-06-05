3dde6c0bd8498aa05fa9288784fe54b0d4a42219
Oleg Proshin <opp20071980@gmail.com>
2014-06-05 16:06:35 +0400

    Refs #3621: Activate alert - after closing window redirect to child/list
    --HG--
    branch : features

--- webroot/js/activate_alert.js
+++ webroot/js/activate_alert.js
@@ -31,6 +31,7 @@ $(function() {
                    open: function() {
                    },
                    close : function() {
+                       window.location = '/child/list';
                    }
                });
 
